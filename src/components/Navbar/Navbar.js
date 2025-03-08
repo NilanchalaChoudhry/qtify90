cd4e5065b0952c8f9201e2d44bc68ed95f2b6b6b
NilanchalaChoudhry <v.choudhury12@gmail.com>
2025-03-08 22:07:21 +0530

    Initial commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,16 @@
+import Logo from "../Logo/Logo";
+import styles from "./Navbar.module.css";
+import Button from "../Button/Button";
+import SearchBar from "../Search/Search";
+
+const NavBar = ({ data, setFeedbackFlag }) => {
+  return (
+    <nav className={styles.navbar}>
+      <Logo />
+      <SearchBar data={data} placeholder="Search a song of your choice" />
+      <Button text="Give Feedback" setFeedbackFlag={setFeedbackFlag} />
+    </nav>
+  );
+};
+
+export default NavBar;
